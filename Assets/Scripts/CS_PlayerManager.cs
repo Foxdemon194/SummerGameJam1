2ae232e6b0a35166a8e208245ec4d728f9aac17b
Foxdemon194 <109841490+Foxdemon194@users.noreply.github.com>
2025-06-24 15:04:35 -0400

    Started working on player animations

---
+++ Assets/Scripts/CS_PlayerManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_PlayerManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlayerStateChange(string newState)
+    {
+        switch(newState)
+        {
+            case "one":
+                {
+
+                    break;
+                }
+            case "two":
+
+                {
+                    break;
+                }
+            default: 
+                {
+                    break;
+                }
+        }
+    }
+}
